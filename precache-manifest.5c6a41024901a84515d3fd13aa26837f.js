83ad5c9802987294c0daedd31abc216c51960412
Tim Udoma <timudoma@Tim-Udomas-MacBook-Pro.local>
2019-02-13 15:07:27 +0100

    Updates

---
+++ precache-manifest.5c6a41024901a84515d3fd13aa26837f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "edabe945cd0b14d093bb",
+    "url": "/contact-manager/static/js/main.edabe945.chunk.js"
+  },
+  {
+    "revision": "48dbd94204b9dfa91768",
+    "url": "/contact-manager/static/js/1.48dbd942.chunk.js"
+  },
+  {
+    "revision": "edabe945cd0b14d093bb",
+    "url": "/contact-manager/static/css/main.b0e51fe6.chunk.css"
+  },
+  {
+    "revision": "48dbd94204b9dfa91768",
+    "url": "/contact-manager/static/css/1.4d3dc990.chunk.css"
+  },
+  {
+    "revision": "6ee4bb5a5ee38d8704021c683787824c",
+    "url": "/contact-manager/index.html"
+  }
+];
