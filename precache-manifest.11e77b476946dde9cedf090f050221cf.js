2f2923c3fe3b367ae7e3b3cd628a2b34f0c75c85
Tim Udoma <timudoma@Tim-Udomas-MacBook-Pro.local>
2019-02-13 15:23:29 +0100

    Updates

--- precache-manifest.11e77b476946dde9cedf090f050221cf.js
+++ precache-manifest.11e77b476946dde9cedf090f050221cf.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
   },
   {
-    "revision": "99a264d8476a8d45d844",
-    "url": "/contact-manager/static/js/main.99a264d8.chunk.js"
+    "revision": "225d56180955c3ef661d",
+    "url": "/contact-manager/static/js/main.225d5618.chunk.js"
   },
   {
-    "revision": "48dbd94204b9dfa91768",
-    "url": "/contact-manager/static/js/1.48dbd942.chunk.js"
+    "revision": "6333b94bff2bc85f8ad2",
+    "url": "/contact-manager/static/js/1.6333b94b.chunk.js"
   },
   {
-    "revision": "99a264d8476a8d45d844",
+    "revision": "225d56180955c3ef661d",
     "url": "/contact-manager/static/css/main.b0e51fe6.chunk.css"
   },
   {
-    "revision": "48dbd94204b9dfa91768",
+    "revision": "6333b94bff2bc85f8ad2",
     "url": "/contact-manager/static/css/1.4d3dc990.chunk.css"
   },
   {
-    "revision": "d491c15507f9fa2ebf1055b1d5c7c8df",
+    "revision": "34cbf12a9443c11fea0a36bdfcd16958",
     "url": "/contact-manager/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/contact-manager/precache-manifest.961cffbba26d93ae58d2848926481acd.js"
+  "/contact-manager/precache-manifest.11e77b476946dde9cedf090f050221cf.js"
 );
 
 workbox.clientsClaim();
